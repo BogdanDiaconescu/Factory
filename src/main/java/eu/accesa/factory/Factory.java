eef3564a46ec71608b1b1aedbf8dabe4f3bf6825
dianaadumitru <109727374+dianaadumitru@users.noreply.github.com>
2022-07-21 14:09:45 +0300

    added headers factory

--- src/main/java/eu/accesa/factory/Factory.java
+++ src/main/java/eu/accesa/factory/Factory.java
@@ -8,4 +8,56 @@ public class Factory {
     public void initialize(Stock stock) {
         System.out.println("factory");
     }
+
+    Boolean checkIfCarCanBeMade(String brand, String model) {
+        return true;
+    }
+
+    Boolean addAirConditioning() {
+        return true;
+    }
+
+    Boolean addAllWheelDrive() {
+        return true;
+    }
+
+    Boolean addAutoPilot() {
+        return true;
+    }
+
+    Boolean addAirBrake() {
+        return true;
+    }
+
+    Boolean addAirChassis() {
+        return true;
+    }
+
+    Boolean addEngine() {
+        return true;
+    }
+
+    Boolean addPart() {
+        return true;
+    }
+
+    Boolean addSeat() {
+        return true;
+    }
+
+    Boolean addSensor() {
+        return true;
+    }
+    Boolean addSteeringWheel() {
+        return true;
+    }
+
+    Boolean addSunRoof() {
+        return true;
+    }
+
+    Boolean addWheel() {
+        return true;
+    }
+
 }

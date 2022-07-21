02a23330e3396e119d706377602ce0c2294ff0a5
Catalin Ionita <catalin.ionita@accesa.eu>
2022-07-21 16:21:09 +0300

    answer stock queries, take part, estimate time

--- src/main/java/eu/accesa/stock/Stock.java
+++ src/main/java/eu/accesa/stock/Stock.java
@@ -3,43 +3,84 @@ package eu.accesa.stock;
 import eu.accesa.internshipfactory.constatnts.Constants;
 import eu.accesa.internshipfactory.constatnts.Parts;
 import eu.accesa.part.Part;
+import org.assertj.core.groups.Tuple;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 
 public class Stock {
-
     private Double steel;
-    private Map<Parts, List<Part>> parts;
-
+    // otel, pret, cantitate
+    private List<List<Double>> parts;
     private SteelGenerator steelGenerator;
-
     private Queue<Part> partQueue;
 
     public void initialize() {
-        System.out.println("stock");
+        System.out.println("stock initializer");
+        parts = new ArrayList<>();
+        parts.add(List.of(10.3d, 4d, 3d));
+        parts.add(List.of(10.3d, 4d, 5d));
+        parts.add(List.of(12d, 4d, 5d));
+        parts.add(List.of(13d, 3d, 5d));
+        parts.add(List.of(14.3d, 4d, 5d));
+
         this.setSteel(Constants.INITIAL_STEEL);
-
-
-    }
-
-
-
-    public Double getSteel() {
-        return steel;
     }
 
     public void setSteel(Double steel) {
         this.steel = steel;
     }
 
-    public Map<Parts, List<Part>> getParts() {
-        return parts;
+    public Boolean checkIfPartInStock(Part part) {
+        // check if the part is in stock list
+        boolean response = false;
+        for (List<Double> miniPart : parts) {
+            if (Objects.equals(miniPart.get(0), part.getSteelContentInKg()) &&
+                    Objects.equals(miniPart.get(1), part.getPrice()) && miniPart.get(2) > 0) {
+                response = true;
+            }
+        }
+
+        if (part.getSteelContentInKg() <= steel) {
+            response = true;
+        }
+
+        return response;
     }
 
-    public void setParts(Map<Parts, List<Part>> parts) {
-        this.parts = parts;
+    public Double getSteel() {
+        return steel;
+    }
+
+    public Boolean takePart(Part part) {
+        boolean ok = false;
+
+        for (List<Double> miniPart : parts) {
+            if (Objects.equals(miniPart.get(0), part.getSteelContentInKg()) &&
+                    Objects.equals(miniPart.get(1), part.getPrice()) && miniPart.get(2) > 0) {
+                miniPart.set(2, miniPart.get(2) - 1);
+                ok = true;
+            }
+        }
+
+        if (!ok && steel >= part.getSteelContentInKg()) {
+            steel -= part.getSteelContentInKg();
+            ok = true;
+        }
+
+        return ok;
+    }
+
+    public Integer checkPartTimeToCreate(Part part) {
+        double requiredSteel = part.getSteelContentInKg() - steel;
+        if(requiredSteel < 0) {
+            return 0;
+        } else {
+            return (int) requiredSteel * 30 / 100;
+        }
+    }
+
+    public void generateSteel() {
+        steel += 10;
     }
 
 }
---
+++ src/main/java/eu/accesa/stock/Test.java
@@ -0,0 +1,18 @@
+package eu.accesa.stock;
+
+import eu.accesa.part.Part;
+
+public class Test {
+    public static void main(String[] args) {
+        Stock stock = new Stock();
+        stock.initialize();
+
+        if (stock.checkIfPartInStock(new Part(3.3d, 4d))) {
+            System.out.println("dadadad");
+        }
+
+        stock.takePart(new Part(3000d, 4d));
+        System.out.println(stock.getSteel());
+        System.out.println(stock.checkPartTimeToCreate(new Part(120d, 4d)));
+    }
+}

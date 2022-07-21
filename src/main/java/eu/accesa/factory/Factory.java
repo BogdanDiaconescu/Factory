a627078eac742430e6bc89003f0480d0d0f524c8
dianaadumitru <109727374+dianaadumitru@users.noreply.github.com>
2022-07-21 16:37:23 +0300

    Merge branch 'checkCar' of C:\Users\diana.dumitru\Desktop\java projects\carsFactory\Factory with conflicts.

--- src/main/java/eu/accesa/factory/Factory.java
+++ src/main/java/eu/accesa/factory/Factory.java
@@ -6,7 +6,6 @@ import eu.accesa.internshipfactory.constatnts.EngineType;
 import eu.accesa.car.Car;
 
 
-
 import eu.accesa.part.Brake;
 import eu.accesa.part.Chassis;
 
@@ -17,14 +16,9 @@ import eu.accesa.part.Engine;
 import eu.accesa.part.SteeringWheel;
 import eu.accesa.part.SunRoof;
 import eu.accesa.part.Wheel;
-
-import eu.accesa.part.*;
-
-
 import eu.accesa.stock.Stock;
 import org.springframework.stereotype.Component;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
@@ -44,29 +38,20 @@ public class Factory {
     }
 
     Boolean addAirConditioning(Integer vents) {
-        AirConditioning part = new AirConditioning(vents);
-        if(!stock.checkIfPartInStock(part)) {
-            return false;
-        }
-        currentCar.setAirConditioning(part);
+        // verify stock
+        currentCar.setAirConditioning(new AirConditioning(vents));
         return true;
     }
 
     Boolean addAllWheelDrive(Integer performance) {
-        AllWheelDrive part = new AllWheelDrive(performance,0.0);
-        if(!stock.checkIfPartInStock(part)) {
-            return false;
-        }
-        currentCar.setAllWheelDrive(part);
+        // verify stock
+        currentCar.setAllWheelDrive(new AllWheelDrive(performance,0.0));
         return true;
     }
 
     Boolean addAutoPilot(Integer level) {
-        AutoPilot part = new AutoPilot(level);
-        if(!stock.checkIfPartInStock(part)) {
-            return false;
-        }
-        currentCar.setAutoPilot(part);
+        // verify stock
+        currentCar.setAutoPilot(new AutoPilot(level));
         return true;
     }
 
@@ -77,7 +62,8 @@ public class Factory {
 
         list.add(b);
 
-        if(stock.checkIfPartInStock(b)){
+        //stock checking in the future
+        if(true){
             currentCar.setBrakes(list);
             return false;
         }
@@ -87,8 +73,8 @@ public class Factory {
     Boolean addChassis(Integer stiffness) {
 
         Chassis c = new Chassis(stiffness);
-
-        if(stock.checkIfPartInStock(c)){
+        //stock checking in the future
+        if(true){
             currentCar.setChassis(c);
             return true;
         }
@@ -111,36 +97,18 @@ public class Factory {
         }
     }
 
-    Boolean addSeat(Double price ,Boolean heated, Boolean manual, Boolean electric) {
-        if(/*verificare*/true)
-        {
-            currentCar.setSeats((List<Seat>) new Seat(price, heated, manual, electric));
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+    Boolean addSeat(Boolean heated, Boolean manual, Boolean electric) {
+        return true;
     }
 
-    Boolean addSensor(Double price) {
-        if(/*verificare*/true)
-        {
-            currentCar.setSensors((List<Sensor>) new Sensor(price));
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+    Boolean addSensor() {
+        return true;
     }
     Boolean addSteeringWheel(String shape) {
         //verify stock
-        SteeringWheel steeringWheel= new SteeringWheel(shape);
-        if (Stock.checkIfPartInStock(steeringWheel)==false)
-            return false;
 
         //add steering wheel
+        SteeringWheel steeringWheel= new SteeringWheel(shape);
         currentCar.setSteeringWheel(steeringWheel);
 
         //return true if added
@@ -149,11 +117,9 @@ public class Factory {
 
     Boolean addSunRoof(Boolean electric) {
         //verify stock
-        SunRoof sunroof=new SunRoof(electric, 100d);
-        if (Stock.checkIfPartInStock(sunroof)==false)
-            return false;
 
         //add sunroof if available
+        SunRoof sunroof=new SunRoof(electric, 100d);
         currentCar.setSunRoof(sunroof);
 
         //return true if added sunroof
@@ -162,11 +128,9 @@ public class Factory {
 
     Boolean addWheel(Integer numberOfSpokes) {
         //verify stock
-        Wheel wheel=new Wheel(numberOfSpokes);
-        if (Stock.checkIfPartInStock(wheel)==false)
-            return false;
 
         //add wheel in the list of wheels if available
+        Wheel wheel=new Wheel(numberOfSpokes);
         Queue wheels = (Queue) currentCar.getWheels();
         if (currentCar.getWheels().size()<4)
             wheels.add(wheel);
@@ -175,50 +139,47 @@ public class Factory {
             wheels.add(wheel);
         }
         currentCar.setWheels((List<Wheel>) wheels);
-
         //return true if added wheels
         return true;
     }
 
 
 
-    public ArrayList<String> getCars(){
+    public ArrayList<String> getTypes(String car){
 
         ArrayList<String> types = new ArrayList<>();
-        types.add("Dacia");
-        types.add("Tesla");
-        types.add("BMW");
 
-        return types;
-
-
-    }
-
-    public ArrayList<String> getModels(String car){
-
-        ArrayList<String> types = new ArrayList<>();
 
         switch (car){
-            case "Dacia" : {
+            case "car" : {
+                types.add("bmw");
+                types.add("dacia");
+                types.add("tesla");
+                break;
+            }
+            case "dacia" : {
+                types.add("Dacia");
                 types.add("Duster");
                 types.add("Logan");
                 types.add("Spring");
                 break;
             }
-            case "BMW" : {
+            case "bmw" : {
+                types.add("Bmw");
                 types.add("Series1");
                 types.add("Series3");
                 break;
             }
-            case "Tesla" : {
+            case "tesla" : {
                 types.add("Model3");
                 types.add("ModelS");
-
+                types.add("Tesla");
                 break;
             }
 
         }
         return types;
+
     }
 
     public ArrayList<String> offerTypes(Object o){
@@ -226,13 +187,11 @@ public class Factory {
         ArrayList<String> types = new ArrayList<>();
         for(Object obj: o.getClass().getClasses()) {
 
-            types.add(obj.getClass().getSimpleName());
+            types.add(obj.getClass().getName());
         }
 
         return types;
     }
-
-
 
 }
 

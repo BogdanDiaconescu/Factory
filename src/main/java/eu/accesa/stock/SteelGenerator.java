7e8bee397789f6ddae9d639455b78d0f73acb199
Catalin Ionita <catalin.ionita@accesa.eu>
2022-07-21 14:02:45 +0300

    variables for stock class

--- src/main/java/eu/accesa/stock/SteelGenerator.java
+++ src/main/java/eu/accesa/stock/SteelGenerator.java
@@ -14,4 +14,5 @@ public class SteelGenerator {
         }
         return null;
     }
+
 }
--- src/main/java/eu/accesa/stock/Stock.java
+++ src/main/java/eu/accesa/stock/Stock.java
@@ -1,7 +1,45 @@
 package eu.accesa.stock;
 
+import eu.accesa.internshipfactory.constatnts.Constants;
+import eu.accesa.internshipfactory.constatnts.Parts;
+import eu.accesa.part.Part;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
 public class Stock {
+
+    private Double steel;
+    private Map<Parts, List<Part>> parts;
+
+    private SteelGenerator steelGenerator;
+
+    private Queue<Part> partQueue;
+
     public void initialize() {
         System.out.println("stock");
+        this.setSteel(Constants.INITIAL_STEEL);
+
+
     }
+
+
+
+    public Double getSteel() {
+        return steel;
+    }
+
+    public void setSteel(Double steel) {
+        this.steel = steel;
+    }
+
+    public Map<Parts, List<Part>> getParts() {
+        return parts;
+    }
+
+    public void setParts(Map<Parts, List<Part>> parts) {
+        this.parts = parts;
+    }
+
 }

1eb2476d2984bcd7bcc2badb42c68b978faa7a23
sebastianCiobanuAccesa <sebastian.ciobanu@accesa.eu>
2022-07-21 15:33:45 +0300

    first add functions

--- src/main/java/eu/accesa/factory/Factory.java
+++ src/main/java/eu/accesa/factory/Factory.java
@@ -5,6 +5,9 @@ import eu.accesa.internshipfactory.constatnts.EngineType;
 
 import eu.accesa.car.Car;
 
+import eu.accesa.part.AirConditioning;
+import eu.accesa.part.AllWheelDrive;
+import eu.accesa.part.AutoPilot;
 import eu.accesa.stock.Stock;
 import org.springframework.stereotype.Component;
 
@@ -23,14 +26,20 @@ public class Factory {
     }
 
     Boolean addAirConditioning(Integer vents) {
+        // verify stock
+        currentCar.setAirConditioning(new AirConditioning(vents));
         return true;
     }
 
     Boolean addAllWheelDrive(Integer performance) {
+        // verify stock
+        currentCar.setAllWheelDrive(new AllWheelDrive(performance,0.0));
         return true;
     }
 
     Boolean addAutoPilot(Integer level) {
+        // verify stock
+        currentCar.setAutoPilot(new AutoPilot(level));
         return true;
     }
 
